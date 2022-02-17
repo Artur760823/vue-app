9c3f1d314e186847719f3376c66c620921c3c93d
Artur <artur.zajaczkowski76@gmail.com>
2022-02-17 21:54:03 +0100

    Final

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,46 @@
 <template>
-  <h1>Hello World</h1>
+  <div class="item">
+    <p>Nowe todo: {{newItem}}</p>
+    <input type="text" placeholder="todo" v-model="newItem"/>
+    <button @click="addItem">Dodaj zadanie</button>
+  </div>
+
+  <div 
+    class="item"
+    v-bind:class="{
+      completed: item.completed
+    }"  
+    v-for="item in items" 
+    v-bind:key="item.id"
+  >
+    <h2>{{item.title}}</h2>
+    <button v-if="!item.completed" @click="removeItem(item.id)">Zakończone</button>
+  </div>
 </template>
 
 <script>
 
 export default {
-  name: 'App',
+  data(){
+    return {
+      newItem:'',
+      items: [
+      {title: 'Zrobić zakupy', completed: false, id: 1},
+      {title: 'Kupić prezent', completed: false, id: 2}
+      ]
+    }
+  },
+  methods:{
+    addItem(){
+      this.items.push({title: this.newItem, completed: false, id: Math.random()})
+      this.newItem =''
+    },
+    removeItem(id){
+      const index = this.items.findIndex(el => el.id === id);
+      this.items[index].completed = true;
+      
+    }
+  }
 }
 </script>
 
@@ -17,5 +52,17 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.item{
+  border: 1px solid #cdcdcd;
+  margin: 8px;
+  padding: 10px;
+}
+.completed{
+  opacity: 0.5;
+
+}
+.completed h2{
+  text-decoration: line-through;
 }
 </style>
